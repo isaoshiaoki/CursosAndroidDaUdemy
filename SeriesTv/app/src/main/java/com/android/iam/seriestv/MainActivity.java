5479b6a14b1ec42f0d7b1f26d26edf4da773dc71
isaoshiaoki <isaoshi.aoki@gmail.com>
2020-07-12 06:21:44 -0300

    SeriesTv

---
+++ SeriesTv/app/src/main/java/com/android/iam/seriestv/MainActivity.java
@@ -0,0 +1,54 @@
+package com.android.iam.seriestv;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+public class MainActivity extends Activity {
+
+    private SeekBar seekbar;
+    private ImageView imagemExibicao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekbar = (SeekBar) findViewById(R.id.seekBarId);
+        imagemExibicao = (ImageView) findViewById(R.id.imagemExibicaoId);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int valorProgresso = progress;
+//baseado no progresso sera exibida uma imagem
+                if(valorProgresso == 1){
+                    imagemExibicao.setImageResource( R.drawable.pouco );
+                }else if(valorProgresso == 2){
+                    imagemExibicao.setImageResource( R.drawable.medio );
+                }else if(valorProgresso == 3){
+                    imagemExibicao.setImageResource( R.drawable.muito );
+                }else if(valorProgresso == 4){
+                    imagemExibicao.setImageResource( R.drawable.susto );
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+}
