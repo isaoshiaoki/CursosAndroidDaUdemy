9bb282475a359edfedff082f852bfb92758c3d1c
isaoshiaoki <isaoshi.aoki@gmail.com>
2020-07-12 06:06:09 -0300

    AtmEmpresaConsultoria

---
+++ AtmEmpresaConsultoria/app/src/main/java/com/example/iam/atmempresaconsultoria/EmpresaActivity.java
@@ -0,0 +1,14 @@
+package com.example.iam.atmempresaconsultoria;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EmpresaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empresa);
+    }
+
+}
---
+++ AtmEmpresaConsultoria/app/src/main/java/com/example/iam/atmempresaconsultoria/ServicoActivity.java
@@ -0,0 +1,14 @@
+package com.example.iam.atmempresaconsultoria;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ServicoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servico);
+    }
+
+}
